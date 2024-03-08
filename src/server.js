92a78444e093f3c206ddb5810c7129a86bdcc28e
Shashi Kant <skntjee@gmail.com>
2024-03-09 00:20:51 +0530

    added fie

--- src/server.js
+++ src/server.js
@@ -1,33 +1,38 @@
-import  Express  from "express";
+import Express from "express";
 import dotenv from "./dotenv";
 import globalMiddleware from "./global_middlware/middleware";
 import { SuccessStatus } from "./response_utils";
 import { initDb } from "./connection/db_connect";
-
-// __dirname 
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import fs from "fs";
+// __dirname
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import path from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
-// __dirname 
+// __dirname
 
-
-
-let app = Express()
+let app = Express();
 
 // console.log("dotenv.DB" ,dotenv.DB_URL)
 
-await initDb()
+await initDb();
 
-app.get('/check-health' ,(req, res)=>{
-    console.log("health check ok !!")
-       res.send(SuccessStatus([],"heath check success"))
-   })
+app.get("/check-health", (req, res) => {
+  console.log("health check ok !!");
+  res.send(SuccessStatus([], "heath check success"));
+});
 
-globalMiddleware(app) 
+globalMiddleware(app);
 
+// folder intiation portion
+let assets = path.join(__dirname, "assets/Logo/categories_logo");
 
+if (!fs.existsSync(assets)) {
+  fs.mkdirSync(assets, { recursive: true });
+}
 
+// folder intiation portion
 
-app.listen(dotenv.PORT ,()=>[
-     console.log(`server started at ${dotenv.PORT} `)
-])
+app.listen(dotenv.PORT, () => [
+  console.log(`server started at ${dotenv.PORT} `),
+]);
