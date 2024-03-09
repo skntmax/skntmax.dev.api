d13c80463a46ce2107fe58c82d24da0286529ad5
Shashi Kant <skntjee@gmail.com>
2024-03-09 12:36:16 +0530

    added

--- src/global_middlware/middleware.js
+++ src/global_middlware/middleware.js
@@ -2,7 +2,7 @@ import { category_router } from "../routers/category/route";
 import express from "express";
 import fileupload from "express-fileupload";
 import path from "node:path";
-
+import cors from "cors";
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { rootRouter } from "../routers/root_router";
@@ -12,6 +12,7 @@ function globalMiddleware(app) {
   app.use(express.static(path.join(__dirname, "../assets/Logo/")));
   app.use(fileupload());
   app.use(express.json());
+  app.use(cors());
 
   rootRouter(app);
 }
